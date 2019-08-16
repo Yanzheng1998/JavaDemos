1f13e66ae888092db3e91648b0481f9939e26bf7
Yanzheng Li <you@example.com>
2019-08-17 00:41:38 +0800

    full comparation on 20000 times appends

--- src/com/company/string/Comparation.java
+++ src/com/company/string/Comparation.java
@@ -1,19 +1,28 @@
 package com.company.string;
 
+import java.util.Date;
+
 public class Comparation {
 
     public static void main(String[] args) {
-        String str1 = "abc";
-        String str2 = "de";
-        long startTime = System.nanoTime();
-        String str = str1 + str2;
-        long endTime = System.nanoTime();
-        System.out.println(endTime - startTime + " nano seconds");
-        StringBuilder stringBuilder1 = new StringBuilder("abc");
-        StringBuilder stringBuilder2 = new StringBuilder("de");
-        startTime = System.nanoTime();
-        StringBuilder stringBuilder = stringBuilder1.append(stringBuilder2);
-        endTime = System.nanoTime();
-        System.out.println(endTime - startTime + " nano seconds");
+        long a = new Date().getTime();
+        String string = "";
+        int n = 20000;
+        for (int i = 0; i < n; i++) {
+            string += "." + i;
+        }
+        System.out.println("String used time " + (System.currentTimeMillis() - a) / 1000.0 + "s");
+        long s1 = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            stringBuilder.append("." + i);
+        }
+        System.out.println("StringBuilder used time " + (System.currentTimeMillis() - s1) / 1000.0 + "s");
+        long s2 = System.currentTimeMillis();
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < n; i++) {
+            stringBuffer.append("." + i);
+        }
+        System.out.println("StringBuffer used time " + (System.currentTimeMillis() - s2) / 1000.0 + "s");
     }
 }
